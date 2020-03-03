bdcdb5fc82099e8735d1f38dc4fa4630e487dbe6
Erin McCarthy <mccarthyern@gmail.com>
2020-03-03 10:26:28 -0800

    fixed some errors that came with adding a new setting

--- Tutorial/gray-scott/monitoring.py
+++ Tutorial/gray-scott/monitoring.py
@@ -11,10 +11,11 @@ from operator import add
 from matplotlib.font_manager import FontProperties
 DEFAULT={
     "graph_layout" : "individual", # or grouped
-    "spatial_time" : "spatial" # or time
-    "memory": { "graph_type" : "line" },
-    "cpu": { "graph_type" : "line"},
-    "io": {"graph_type" : "line"} }
+    "spatial_time" : "spatial", # or time
+    "graphs":
+        { "memory": { "graph_type" : "line" },
+          "cpu": { "graph_type" : "line"},
+          "io": {"graph_type" : "line"} }}
 # Global variables
 cpu_component_count = 9
 cpu_components = ['Guest','I/O Wait', 'IRQ', 'Idle', 'Nice', 'Steal', 'System', 'User', 'soft IRQ']
@@ -54,7 +55,7 @@ def dumperiod_valuesars(vars_info):
             print("\t" + key + ": " + value)
         print("\n")
 
-def initialize_globals():
+def initialize_globals(settings):
     global cpu_components
     global previous_mean
     global previous_count
@@ -107,30 +108,39 @@ def get_utilization(is_cpu, fr_step, vars_info, components, previous_mean, previ
         #print(c,mean_values[0],count_values[0],total_value, current_period[c])
         period_values[c] = np.append(period_values[c], current_period[c])
 
-def get_top5_cpu(fr_step, vars_info):
+def get_top5(fr_step, vars_info):
     num_ranks = int(vars_info["num_threads"]["Shape"].split(',')[0])
     num_threads = fr_step.read("num_threads")[0]
-    cpu_means = {}
-    cpu_values = {}
+    timer_means = {}
+    timer_values = {}
     for name, info in vars_info.items():
         name_split = name.split("/")
         if ".TAU application" in name:
             continue
         if "addr=" in name:
             continue
-        if name_split[0] in cpu_components:
+        if "Exclusive TIME" in name:
             shape_str = vars_info[name]["Shape"].split(',')
-            shape = list(map(int, shape_str)) 
+            shape = list(map(int,shape_str))
             mean_values = fr_step.read(name)
-            cup_values[name_split[0]] = []
-            cpu_values[name_split[0]].append(mean_values[0])
+            shortname = name.replace(" / Exclusive TIME", "")
+            timer_values[shortname] = []
+            timer_values[shortname].append(mean_values[0])
             index = num_threads
             while index < shape[0]:
-                cpu_values[name_split[0]].append(mean_values[index])
+                timer_values[shortname].append(mean_values[index])
                 index = index + num_threads
-            cpu_means[shortname] = np.sum(cpu_values[name_split[0]]) / num_ranks
+            timer_means[shortname] = np.sum(timer_values[shortname]) / num_ranks   
     limit = 0
-    others = len(timer_means)-5
+    others = len(timer_means) - 5
+    timer_values["other"] = [0] * num_ranks
+    for key, value in sorted(timer_means.items(), key=lambda kv: kv[1]):
+        limit = limit + 1
+        if limit <= others:
+            timer_values["other"] = list( map(add, timer_values["other"], timer_values[key]) )
+            del timer_values[key]
+    #print(timer_values)
+    return timer_values, num_ranks
 
 #plot stacked bar chart
 #potentially replace x with a df
@@ -202,47 +212,7 @@ def plot_timers(ax, x, fontsize, top5):
     fontP = FontProperties()
     fontP.set_size('small')
     ax.legend(bbox_to_anchor=(1.10,0.5), loc="center left", borderaxespad=0, prop=fontP)
-#this was left in for easy testing purposes of plot_utilization to make sure they matched
-# if plot_utilization works in every case there is no need for this
-def plot_utilization_two(args, x, fontsize, step, top5, func_dict, data):
-    #this is all of them together
-    print("plotting", end='...', flush=True)
-    all_together = data[graph_layout]
-    if all_together == 'grouped':
-        fig = plt.figure(4, figsize=(8,8), constrained_layout=True)
-        gs = gridspec.GridSpec(4, 1, figure=fig)
-        cpu = fig.add_subplot(gs[0, 0])
-        mem = fig.add_subplot(gs[1, 0])
-        io = fig.add_subplot(gs[2, 0])
-        timers = fig.add_subplot(gs[3, 0])
-        plot_stacked_bar(cpu,x,fontsize,"CPU_Utilization", cpu_components)
-        #plot_cpu_utilization(cpu, x, fontsize)
-        plot_line_graph(mem,x, fontsize, "mem_Utilization", mem_components)
-        #plot_mem_utilization(mem, x, fontsize)
-        plot_io_utilization(io, x, fontsize)
-        plot_timers(timers, np.arange(16), fontsize, top5)
-        plt.tick_params(axis='both', which='both', labelsize = fontsize/2)
-        print("here")
-    else:
-        fig = plt.figure(1, figsize=(8,8), constrained_layout=True)
-        cpu = fig.add_subplot()
-        plot_cpu_utilization(cpu, x, fontsize)
-        fig.savefig(args.outfile+".png")
-        
-    print("writing", end='...', flush=True)
-    #come back to this ion call
-    #plt.ion()
-    if all_together:
-        if (args.outfile == "screen"):
-            plt.show()
-            plt.pause(args.displaysec)
-        else:
-            print("here")
-            imgfile = args.outfile+"_"+"{0:0>5}".format(step)+"dif2.png"
-            fig.savefig(imgfile)
 
-    plt.clf()
-    print("done.") 
 def plot_utilization(aregs, x, fontsize, step, top5, settings):
     func_dict = {"line" : plot_line_graph,
                  "bar"  : plot_bar_chart,
@@ -253,7 +223,7 @@ def plot_utilization(aregs, x, fontsize, step, top5, settings):
     
     print("plotting", end='...', flush=True)
     count = 0
-    total = len(settings.keys())
+    total = len(settings["graphs"].keys())
     SEP = True
     if SEP:
         fig = plt.figure()
@@ -261,13 +231,13 @@ def plot_utilization(aregs, x, fontsize, step, top5, settings):
     else:
         fig = plt.figure(total, figsize=(8,8), constrained_layout=True)
         gs = gridspec.GridSpec(total, 1, figure=fig)
-    for key in settings.keys():
+    for key in settings["graphs"].keys():
         if SEP:
             holder = fig.add_subplot(gs[count,0])
         else:
             holder = fig.add_subplot(gs[count,0])
             count += 1
-        graph_type = settings[key]["graph_type"]
+        graph_type = settings["graphs"][key]["graph_type"]
         func_dict[graph_type](holder, x, fontsize, key + " Utilization",components_dict[key])
         if SEP: 
             imgfile = args.outfile+"_"+"{0:0>5}".format(step)+str(key).capitalize()+".png"
@@ -308,7 +278,7 @@ def process_file(args):
         get_utilization(True, fr_step, vars_info, cpu_components, previous_mean, previous_count, current_period, period_values)
         get_utilization(False, fr_step, vars_info, mem_components, previous_mean, previous_count, current_period, period_values)
         get_utilization(False, fr_step, vars_info, io_components, previous_mean, previous_count, current_period, period_values)
-        top5 = get_top5_cpu(fr_step, vars_info)
+        top5 = get_top5(fr_step, vars_info)
 
         x=range(0,cur_step+1)
         plot_utilization(args, x, fontsize, cur_step, top5, settings)
